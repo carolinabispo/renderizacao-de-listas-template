8e8ad0f36b3b944f0d0c9160631bb136713164ad
carolinabispo <carolinabispos07@gmail.com>
2023-06-07 21:55:30 -0300

    template aula

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -13,35 +13,55 @@ import bin from "../../assets/bin.png";
 export function ListaTarefas() {
   const [novaTarefa, setNovaTarefa] = useState("");
 
-
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
 
-  const adicionaTarefa = () => {};
+  const [lista, setLista] = useState(['tarefa 1', 'tarefa 2', 'tarefa 3']);
 
-  const removeTarefa = () => {};
+  const adicionaTarefa = () => {
+    // const listaNovaTarefa = [...lista, novaTarefa]
+    // setLista(listaNovaTarefa)
+    if(novaTarefa !== ''){
+      setLista([...lista, novaTarefa])
+      setNovaTarefa('')
+    }
+    
+  };
 
+  const removeTarefa = (elementRemover) => {
+   const filterList = lista.filter((tarefa) => tarefa !== elementRemover)
+    setLista(filterList)
+
+  };
+
+  const renderList = lista.map((item, index) => {
+    return (
+      <Tarefa key={index}>
+        <p>{item}</p>
+        <RemoveButton onClick={() => removeTarefa(item)}>
+          <img src={bin} alt="" width="16px" />
+        </RemoveButton>
+      </Tarefa>
+    );
+  });
+
+  
   return (
     <ListaTarefasContainer>
-      <InputContainer>
+      <InputContainer >
         <TaskInput
           placeholder="Digite aqui uma tarefa"
           value={novaTarefa}
           onChange={onChangeTarefa}
+          
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa} >Adicionar</AddTaskButton>
       </InputContainer>
 
       <ListaContainer>
-        <ul>
-          <Tarefa>
-            <p>Nova tarefa</p>
-            <RemoveButton>
-              <img src={bin} alt="" width="16px" />
-            </RemoveButton>
-          </Tarefa>
-        </ul>
+        <ul></ul>
+        {renderList}
       </ListaContainer>
     </ListaTarefasContainer>
   );
